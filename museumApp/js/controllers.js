bbe346ca699264245f21dc1775773384d1e5e6e7
Daniel Ranzi Werle <daniel@nextt.com.br>
2017-03-30 23:15:09 -0300

    Commiting first batch of app

---
+++ museumApp/js/controllers.js
@@ -0,0 +1,98 @@
+angular.module('app.controllers', [])
+     
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('welcomeToTheMuseumAppCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('chooseActivityCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('inWhatMuseumYouAreCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('searchMuseumCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('searchMuseum2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('searchMuseum3Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('selectMuseumCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('bingoGameCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('bingoGame2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ museumApp/js/routes.js
@@ -0,0 +1,89 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/page5',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/page6',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('welcomeToTheMuseumApp', {
+    url: '/page7',
+    templateUrl: 'templates/welcomeToTheMuseumApp.html',
+    controller: 'welcomeToTheMuseumAppCtrl'
+  })
+
+  .state('chooseActivity', {
+    url: '/page8',
+    templateUrl: 'templates/chooseActivity.html',
+    controller: 'chooseActivityCtrl'
+  })
+
+  .state('inWhatMuseumYouAre', {
+    url: '/page9',
+    templateUrl: 'templates/inWhatMuseumYouAre.html',
+    controller: 'inWhatMuseumYouAreCtrl'
+  })
+
+  .state('searchMuseum', {
+    url: '/page11',
+    templateUrl: 'templates/searchMuseum.html',
+    controller: 'searchMuseumCtrl'
+  })
+
+  .state('searchMuseum2', {
+    url: '/page12',
+    templateUrl: 'templates/searchMuseum2.html',
+    controller: 'searchMuseum2Ctrl'
+  })
+
+  .state('searchMuseum3', {
+    url: '/page13',
+    templateUrl: 'templates/searchMuseum3.html',
+    controller: 'searchMuseum3Ctrl'
+  })
+
+  .state('selectMuseum', {
+    url: '/page10',
+    templateUrl: 'templates/selectMuseum.html',
+    controller: 'selectMuseumCtrl'
+  })
+
+  .state('bingoGame', {
+    url: '/page14',
+    templateUrl: 'templates/bingoGame.html',
+    controller: 'bingoGameCtrl'
+  })
+
+  .state('bingoGame2', {
+    url: '/page15',
+    templateUrl: 'templates/bingoGame2.html',
+    controller: 'bingoGame2Ctrl'
+  })
+
+$urlRouterProvider.otherwise('/page7')
+
+  
+
+});
